59703d42d447a6f3729546bd720686705676deff
tristangaebler <tgae6529@740s-pl-115.local>
2016-01-06 09:10:28 -0700

    Creating and committing the iOS about me app

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Gaebler, Tristan on 1/6/16.
+//  Copyright © 2016 Gaebler, Tristan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+ 
+    @IBAction func changeToInterests(sender: UIButton) {
+        performSegueWithIdentifier("changeToInterests", sender: sender)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
